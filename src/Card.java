70dcb17e7dc24c7656e31bedc9e6ef12bbe61685
Lőrincz Szilvia <lorszil@Lorinczs-MacBook-Pro.local>
2017-01-16 10:28:10 +0100

    refactoring and retesting

--- src/Card.java
+++ src/Card.java
@@ -7,29 +7,31 @@ public class Card {
     String cardCode;
     String cardColor;
     String cardValue;
-
-    private final List<String> cardColors = Arrays.asList("S", "C", "D", "H");
-
-    HashMap<String, Integer> alphabeticCardValue = new HashMap<>();
     Integer intCardValue;
 
-
-    public Card(String cardCode) throws IllegalArgumentException {
-
-        this.cardCode = cardCode;
-        this.cardColor = this.cardCode.substring(0, 1).toUpperCase();
-        this.cardValue = this.cardCode.substring(1).toUpperCase();
-
-        if (!cardColors.contains(cardColor))
-        {
-            throw new IllegalArgumentException("Card color isn't valid: " + this.cardColor);
-        }
+    private final static List<String> CARD_COLORS = Arrays.asList("S", "C", "D", "H");
 
 
+    private static HashMap<String, Integer> alphabeticCardValue = new HashMap<>();
+
+    static {
         alphabeticCardValue.put("J", 11);
         alphabeticCardValue.put("Q", 12);
         alphabeticCardValue.put("K", 13);
         alphabeticCardValue.put("A", 14);
+    }
+
+
+    public Card(String cardCode) throws IllegalArgumentException {
+
+        this.cardCode = cardCode.toUpperCase();
+        this.cardColor = this.cardCode.substring(0, 1);
+        this.cardValue = this.cardCode.substring(1);
+
+        if (!CARD_COLORS.contains(cardColor))
+        {
+            throw new IllegalArgumentException("Card color isn't valid: " + this.cardColor);
+        }
 
         if (alphabeticCardValue.get(this.cardValue) == null) {
 
@@ -37,16 +39,12 @@ public class Card {
             if (intCardValue > 10 || intCardValue < 2) {
                 throw new IllegalArgumentException("Card number isn't valid: " + intCardValue);
             }
+        }else {
+            intCardValue = alphabeticCardValue.get(this.cardValue);
         }
     }
 
     public int getValue() {
-
-        if (alphabeticCardValue.get(this.cardValue) == null) {
-            intCardValue = Integer.parseInt(this.cardValue);
-        } else {
-            intCardValue = alphabeticCardValue.get(this.cardValue);
-        }
-        return intCardValue;
+        return this.intCardValue;
     }
 }
